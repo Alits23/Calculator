d80f736fa1bf33582cc1dc6d42935ac3409bfe38
Ali Tashakori <ali.tashakori.23@gmail.com>
2023-05-06 23:08:20 +0330

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,198 @@
 import 'package:calculator/Constants/constants.dart';
 import 'package:flutter/material.dart';
-import 'Row.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var text = '';
+  String result = '';
+
+  void buttonpressed(String box) {
+    setState(() {
+      text = text + box;
+    });
+  }
+
+  Widget row(String box1, String box2, String box3, String box4) {
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          // box 1
+          SizedBox(
+            height: getsizebox(box1),
+            width: getsizebox(box1),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                shape: CircleBorder(
+                  side: BorderSide(width: 0, color: Colors.transparent),
+                ),
+                backgroundColor: getsahpecolor(box1),
+              ),
+              onPressed: () {
+                setState(() {
+                  if (box1 == 'ac') {
+                    text = '';
+                    result = '';
+                  } else {
+                    buttonpressed(box1);
+                  }
+                });
+              },
+              child: Text(
+                box1,
+                style: TextStyle(
+                  fontSize: 26,
+                  color: getcolorOperator(box1),
+                ),
+              ),
+            ),
+          ),
+
+          // box 2
+          SizedBox(
+            height: getsizebox(box2),
+            width: getsizebox(box2),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                shape: CircleBorder(
+                  side: BorderSide(width: 0, color: Colors.transparent),
+                ),
+                backgroundColor: getsahpecolor(box2),
+              ),
+              onPressed: () {
+                setState(() {
+                  if (box2 == 'ce' && text != '') {
+                    text = text.substring(0, text.length - 1);
+                  }
+                  // این شرط برای اینکه اگه متن خالی بود ارور تکست لنگ نده که -1 شده
+                  else if (box2 == 'ce' && text == '') {
+                    text = '';
+                  } else {
+                    buttonpressed(box2);
+                  }
+                });
+              },
+              child: Text(
+                box2,
+                style: TextStyle(
+                  fontSize: 26,
+                  color: getcolorOperator(box2),
+                ),
+              ),
+            ),
+          ),
+
+          // box 3
+          SizedBox(
+            height: getsizebox(box3),
+            width: getsizebox(box3),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                shape: CircleBorder(
+                  side: BorderSide(width: 0, color: Colors.transparent),
+                ),
+                backgroundColor: getsahpecolor(box3),
+              ),
+              onPressed: () {
+                buttonpressed(box3);
+              },
+              child: Text(
+                box3,
+                style: TextStyle(
+                  fontSize: 26,
+                  color: getcolorOperator(box3),
+                ),
+              ),
+            ),
+          ),
+
+          // box 4
+          SizedBox(
+            height: getsizebox(box4),
+            width: getsizebox(box4),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                shape: CircleBorder(
+                  side: BorderSide(
+                    width: 0,
+                    color: Colors.transparent,
+                  ),
+                ),
+                backgroundColor: getsahpecolor(box4),
+              ),
+              onPressed: () {
+                if (box4 == '=') {
+                  Parser parser = Parser();
+                  Expression expression = parser.parse(text);
+                  ContextModel contextModel = ContextModel();
+                  double eval =
+                      expression.evaluate(EvaluationType.REAL, contextModel);
+                  setState(() {
+                    result = eval.toString();
+                  });
+                } else {
+                  buttonpressed(box4);
+                }
+              },
+              child: Text(
+                box4,
+                style: TextStyle(
+                  fontSize: 26,
+                  color: getcolorOperator(box4),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  bool Operator(String text) {
+    var list = ['ac', 'ce', '%', '/', '*', '-', '+', '='];
+    for (var element in list) {
+      if (text == element) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  Color getsahpecolor(String box) {
+    if (Operator(box)) {
+      return backgroundGreyDark;
+    } else {
+      return backgroundGrey;
+    }
+  }
+
+  Color getcolorOperator(String box) {
+    if (Operator(box)) {
+      return textGreen;
+    } else {
+      return textGrey;
+    }
+  }
+
+  double getsizebox(String box) {
+    if (Operator(box)) {
+      return 60;
+    } else {
+      return 60;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +206,33 @@ class MyApp extends StatelessWidget {
               flex: 4,
               child: Container(
                 color: backgroundGreyDark,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 20),
+                      child: Text(
+                        text,
+                        style: TextStyle(fontSize: 40, color: textGreen),
+                        textAlign: TextAlign.right,
+                      ),
+                    ),
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(top: 2, right: 10, bottom: 10),
+                      child: Text(
+                        result,
+                        style: TextStyle(
+                          fontSize: 50,
+                          color: textGrey,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.right,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
             Expanded(
@@ -29,11 +241,11 @@ class MyApp extends StatelessWidget {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    row('ac', 'ce', '%', '÷'),
-                    row('7', '8', '9', '×'),
+                    row('ac', 'ce', '%', '/'),
+                    row('7', '8', '9', '*'),
                     row('4', '5', '6', '-'),
                     row('1', '2', '3', '+'),
-                    row('00', '0', '∙', '='),
+                    row('00', '0', '.', '='),
                   ],
                 ),
                 color: backgroundGrey,
